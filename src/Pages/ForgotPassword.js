72cc3a35d36293cd51d389e0b1045f9d94f63fe8
Rosakab <akkabiri2003@gmail.com>
2023-12-31 01:02:16 -0800

    create signin/signup forms

--- src/Pages/ForgotPassword.js
+++ src/Pages/ForgotPassword.js
@@ -1,9 +1,71 @@
-import React from 'react'
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-const ForgotPassword = () => {
+import OAuth from "../components/OAuth";
+
+export default function ForgotPassword() {
+  const [email, setEmail] = useState("");
+
+  function onChange(e) {
+    setEmail(e.target.value);
+  }
+
   return (
-    <div>ForgotPassword</div>
-  )
-}
+    <section>
+      <h1 className="text-3xl text-center mt-6 font-bold">
+        Reset Your Password
+      </h1>
+      <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
+        <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
+          <img
+            src="https://images.pexels.com/photos/7579200/pexels-photo-7579200.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="key"
+            className="w-full rounded-2xl"
+          />
+        </div>
+        <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
+          <form>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Email address"
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+            />
 
-export default ForgotPassword
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+              <p className="mb-6">
+                Don't have an account?
+                <Link
+                  to="/sign-up"
+                  className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1"
+                >
+                  Sign up
+                </Link>
+              </p>
+              <p>
+                <Link
+                  to="/sign-in"
+                  className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out"
+                >
+                  Sign in instead
+                </Link>
+              </p>
+            </div>
+            <button
+              className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium  rounded shadow-md hover:bg-blue-700 "
+              type="submit"
+            >
+              Reset password
+            </button>
+            <div className="flex items-center  my-4 before:border-t before:flex-1 before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
+              <p className="text-center font-semibold mx-4">OR</p>
+            </div>
+            <OAuth />
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/OAuth.js
@@ -0,0 +1,10 @@
+export default function OAuth() {
+  return (
+    <button
+      type="button"
+      className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3  text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg  rounded"
+    >
+      Sign in with Google
+    </button>
+  );
+}
