547738bd0dec8486dd30f4fc3c74049f3222f6e2
Rosakab <akkabiri2003@gmail.com>
2024-01-01 20:13:09 -0800

    create private route and custome hook for protecting the profile page and add the logout functionality

--- src/App.js
+++ src/App.js
@@ -1,55 +1,48 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home"; 
-import ForgotPassword from "./Pages/ForgotPassword"; 
-import Offers from "./Pages/Offers"; 
-import Profile from "./Pages/Profile"; 
-import SignIn from "./Pages/SignIn"; 
-import SignUp from "./Pages/SignUp"; 
+import Home from "./Pages/Home";
+import ForgotPassword from "./Pages/ForgotPassword";
+import Offers from "./Pages/Offers";
+import Profile from "./Pages/Profile";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
 import Header from "./components/Header";
-import {ToastContainer} from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
-   <div>
-<BrowserRouter>
-<Header/>
-<Routes>
-  <Route  path="/"  element={<Home/>} />
-  <Route  path="/Profile"  element={<Profile/>} />
-  <Route  path="/Sign-in"  element={<SignIn/>} />
-  <Route  path="/Sign-up"  element={<SignUp/>} />
-  <Route  path="/Offers"  element={<Offers/>} />
-  <Route  path="/Forgot-password"  element={<ForgotPassword/>} />
-</Routes>
-</BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
 
-<ToastContainer 
-position="top-center"
-autoClose={5000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="dark"
-/>
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
 
+          <Route path="/Sign-in" element={<SignIn />} />
+          <Route path="/Sign-up" element={<SignUp />} />
+          <Route path="/Offers" element={<Offers />} />
+          <Route path="/Forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </BrowserRouter>
 
-
-
-
-
-
-   </div>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </div>
   );
 };
-
-
 
 export default App;
